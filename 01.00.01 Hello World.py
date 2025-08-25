271b003ee9696d37deda2b975ff4f5a7069d8bf9
SirWiltfang <slwiltfang@ualr.edu>
2025-08-25 14:31:26 +0000

    day 2 of class

---
+++ 01.00.01 Hello World.py
@@ -0,0 +1,41 @@
+print("Hello World")
+#first day o' class
+
+#This is a comment
+print("Hello, World")
+#You can have a comment after a line of code 
+print("Hello, World") #This is a comment
+#You can "comment out" a line of code to prevent it from executing 
+#print("Good Morning")
+print("Hello, World")
+
+# A variable is created the moment you first assign a value to it. 
+x = 5 
+y = "John" 
+print(x) 
+print(y)
+ 
+# Variables do not need to be declared with any particular type and # can even change type after they have been set.
+x = 4       # x is of type integer 
+print(x) 
+x = "Sally" # x is now of type string 
+print(x)
+ 
+# String variables can be declared either by # using single or double quotes:
+x = "John" 
+print(x) 
+# is the same as 
+x = 'John' 
+print(x)
+ 
+# Python allows you to assign values to multiple variables in one line:
+x, y, z = "Orange", "Banana", "Cherry" 
+print(x) 
+print(y) 
+print(z)
+ 
+# And you can assign the same value to multiple variables in one line:
+x = y = z = "Orange" 
+print(x) 
+print(y) 
+print(z)
