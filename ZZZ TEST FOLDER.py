bb1f13aa268e48d1aec4a2ad446542715c443e27
SirWiltfang <slwiltfang@ualr.edu>
2025-10-01 05:33:54 +0000

    jjjjjjjjj

--- ZZZ TEST FOLDER.py
+++ ZZZ TEST FOLDER.py
@@ -1,38 +1,46 @@
-def merge(input , merger , output):
+def merge(input_path, merger_path, output_path):
 
     input_o = open(input_path, 'r')
     merger_o = open(merger_path, 'r')
     outputw = open(output_path, 'w')
 
-##########################
+    a = -1
+    for line in input_o:
+        a += 1
+    input_o.seek(0) 
 
-    input_c = input_o.read()
-    merger_c = merger_o.read()
+    b = 0
+    for line in merger_o:
+        b += 1
+    merger_o.seek(0)
     
-    x = input_c.find('**')
-    y = input_c.rfind('**')+2
-    f = input_c.replace(input_c[x:y] , merger_c)
+    A= str(a)
+    B= str(b)
+    print( A + " input file records")
+    print( B + " merge file records")
 
-##############################
+    ##########################
+    
+    for line in input_o:
+        if "**Insert Merge File Here**" in line:
+            for m_line in merger_o:
+                outputw.write(m_line)
+        else:
+            outputw.write(line)
 
-    outputw.write(f)
+    ##############################
+
     outputw.close()
     
-    input_o.seek(0)
-    merger_o.seek(0)
-
-
-    a = len(input_o.readlines()) -1
-    b = len(merger_o.readlines())
-    c = len(outputw.readlines())
-    print(a)
-    print(b)
-    print(c)
+    outputr = open(output_path, 'r')
+    c = len(outputr.readlines())
+    C= str(c)
+    print( C + " output file records")
 
     input_o.close()
     merger_o.close()
-    outputr.close()
-
+    outputr.close() 
+    
 ##################################
 
-merge("06.Project Input File.txt" , "06.Project Merge File.txt" ,  "06.Project Output File.txt")
+merge("06.Project Input File.txt", "06.Project Merge File.txt", "06.Project Output File.txt")
