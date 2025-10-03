84fe403b92992ac89ad788b86d05c9ddbd30b23a
SirWiltfang <slwiltfang@ualr.edu>
2025-10-03 20:51:06 +0000

    j

--- 07.Project/07.Project.py
+++ 07.Project/07.Project.py
@@ -1,7 +1,31 @@
 
 def Convert(inputfile, outputfile):
 
-    with open(inputfile, "r") as file:
-        print(file)
+    OUTLINES = 0
+    with open(inputfile, "r") as infile:
+        with open(outputfile, "w") as outfile:
 
-Convert("07.Project/07.Project Angles Input.txt")
+            for line in infile:
+                OUTLINES += 1
+
+                x = line.find(chr(176))
+                Deg= int((line[:x]))
+
+                y = line.find("'")
+                Min= int(line[x+1:y])
+                dem1 = Min/60
+
+                z = line.find('"')
+                Sec= int(line[y+1:z])
+                dem2 = Sec/3600
+                
+                FinalDeg = Deg + dem1 + dem2
+                Final = str(FinalDeg)
+
+                outfile.write(Final+"\n")
+
+    print(f"\n{OUTLINES} records processed\n")
+        
+
+
+Convert("07.Project/07.Project Angles Input.txt", "07.Project/07.Project Angles Output.txt" )
--- ZZZ TESTING.py
+++ ZZZ TESTING.py
@@ -1,5 +1,19 @@
-x=5
-while x>0:
-    print(x)
-    x = x-1
-print("blast off")
+
+def Convert(inputfile, outputfile):
+
+    with open(inputfile, "r") as infile, open(outputfile, "w") as outfile:
+        OUTLINES = 0
+        
+        while OUTLINES <=7 :
+            line=str(infile.readline())
+
+            
+            outfile.write(line)
+
+            for line in infile:
+                OUTLINES += 1
+        
+        print(f"Conversion complete. Total lines written: {OUTLINES}")
+
+
+Convert("07.Project/07.Project Angles Input.txt", "07.Project/07.Project Angles Output.txt" )
