93401a0caca9b3323c8e20243a2a0992355dc312
SirWiltfang <slwiltfang@ualr.edu>
2025-10-03 19:09:36 +0000

    g

---
+++ 07.Project/07.Project.py
@@ -0,0 +1,7 @@
+
+def Convert(inputfile, outputfile):
+
+    with open(inputfile, "r") as file:
+        print(file)
+
+Convert(07.Project/07.Project Angles Input.txt)
---
+++ ZZZ STATEMENTS.py
@@ -0,0 +1,25 @@
+
+##########################################
+#if, elif, else
+age = int(input("Enter your age:"))
+
+if age >= 100:
+    print("You are too old")
+
+elif age >= 18:
+    print("you are old enough")
+
+elif age < 0:
+    print("you must be 18+")
+
+else:
+    print("too young")
+##############################################
+#WHILE LOOP
+x=5
+while x>0:
+    print(x)
+    x = x-1
+print("blast off")
+#####################################
+#for ___ in ___
