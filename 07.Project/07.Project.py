12116918ecdad735750f25422fbe0b541ab9a40c
SirWiltfang <slwiltfang@ualr.edu>
2025-10-03 20:50:59 +0000

    j

--- 07.Project/07.Project.py
+++ 07.Project/07.Project.py
@@ -4,4 +4,4 @@ def Convert(inputfile, outputfile):
     with open(inputfile, "r") as file:
         print(file)
 
-Convert(07.Project/07.Project Angles Input.txt)
+Convert("07.Project/07.Project Angles Input.txt")
