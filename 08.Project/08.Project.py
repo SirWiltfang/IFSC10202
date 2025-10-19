c6989d800c45efbd73cf703eed8f1314d6d2b291
SirWiltfang <slwiltfang@ualr.edu>
2025-10-19 20:24:26 +0000

    h

--- 08.Project/08.Project.py
+++ 08.Project/08.Project.py
@@ -1,8 +1,6 @@
 
 import sys
 
-################################################
-
 def filesearch(file):
 
     with open(file, "r") as openfile:
@@ -19,35 +17,23 @@ def filesearch(file):
         else:
             pass
 
-################################################################
-
             last_printed = -1
-
             for index, line in enumerate(FILE_LINES):
-                
+            
                 if search in line:
-
                     if index > last_printed:
-
                         start_index = index
-
                         while start_index > 0 and FILE_LINES[start_index -1] != "":
                             start_index-=1
-                        
                         if start_index > 0 and FILE_LINES[start_index -1] == "":
                             start_index -= 1 
 
-#################################
-
                         end_index = index
-
                         while end_index < linecount and FILE_LINES[end_index] != "":
                             end_index+=1
                         if end_index < linecount:
-                            end_index+=1 
+                            end_index+=1
 
-#############################################
-                        
                         for section_num in range(start_index , end_index):
                             line_content = FILE_LINES[section_num]
                             print("line" , section_num , ":" , line_content)
