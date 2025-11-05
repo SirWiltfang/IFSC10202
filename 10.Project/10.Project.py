82be32a70a6099a89835732e146f0969dc1245b5
SirWiltfang <slwiltfang@ualr.edu>
2025-11-05 18:05:25 +0000

    gg

--- 10.Project/10.Project.py
+++ 10.Project/10.Project.py
@@ -1,17 +1,43 @@
 
-def Grading(file):
-    
- 
-   
+import math
+import os
+
+FILEPATH = "10.Project/10.Project Student Scores.txt"
+
+############################################
+
+class Student:
+
+    def __init__ (self, firstname, lastname, tnumber, score):
+        
+        self.FirstName = firstname
+        self.LastName = lastname
+        self.TNumber = tnumber
+        self.Grades = score
+
+
+    def runningaverage(self):
+        Scores = []
         
 
+##########################################
+#GRADIN
 
-    class Student:
-        def __init__ (self, firstname, lastname, tnumber, score):
-            self.firstname = firstname
-            self.lastname = lastname
-            self.tnumber = tnumber
-            self.score = score
+    def L_grading(self):
 
+        average = self.total_average()
+
+        if average >= 90:
+            return "A"
+        elif average >= 80:
+            return "B"
+        elif average >= 70:
+            return "C"
+        elif average >= 60:
+            return "D"
+        else:
+            return "F"
+            
+################################################
 
 
