9e953635b341442978dd904ece83816da3cc01ca
SirWiltfang <slwiltfang@ualr.edu>
2025-11-12 20:54:37 +0000

    gg

--- 11.Project/11.Project.py
+++ 11.Project/11.Project.py
@@ -52,3 +52,99 @@ class Student:
             return "F"
         
 #######################################
+
+class StudentList: 
+    
+    def __init__ (self):
+        self.Studentlist = []
+
+#######################################
+
+    def add_student(self, firstname, lastname, tnumber, score=None):
+        
+        newstudent = Student(firstname, lastname, tnumber, score) 
+        self.Studentlist.append(newstudent)
+
+#######################################
+
+    def find_student(self, tnumber):
+
+        for index, student in enumerate(self.Studentlist): 
+            if student.TNumber == tnumber:
+                return index
+        return -1
+
+#######################################
+
+    def print_student_list(self):
+
+        HEADER = (
+            f"\n{'First':>12}{'Last':>12}{'ID':>12}{'Running':>12}{'Semester':>12}{'Letter':>12}\n"
+            f"{'Name':>12}{'Name':>12}{'Number':>12}{'Average':>12}{'Average':>12}{'Grade':>12}\n"
+            f"{'-'*12}{' '}{'-'*11}{' '}{'-'*11}{' '}{'-'*11}{' '}{'-'*11}{' '}{'-'*11}" 
+        )
+        print(HEADER)
+
+##################3333
+
+        for student in self.Studentlist:
+            running_avg = student.RunningAverages() 
+            total_avg = student.TotalAverage()
+            letter_grade = student.LetterGrade()
+
+            output = (
+                        f"{student.FirstName:>12}"
+                        f"{student.LastName:>12}"
+                        f"{student.TNumber:>12}"
+                        f"{running_avg:>12.2f}"
+                        f"{total_avg:>12.2f}"
+                        f"{letter_grade:>12}"
+                        )
+            print(output)
+
+#######################################
+
+    def add_student_from_file(self, filename):
+        with open(filename, 'r') as openfile:
+            for line in (openfile):
+                if not line.strip():
+                    continue
+
+                parts = line.strip().split(',')
+
+                if len(parts) >= 3:
+                    first_name, last_name, t_number = [p.strip() for p in parts[:3]]
+                    self.add_student(first_name, last_name, t_number, None) 
+
+#######################################
+  
+    def add_scores_from_file(self, filename):
+        with open(filename, 'r') as openfile:
+            for line in openfile:
+                if not line.strip():
+                    continue
+
+                parts = line.strip().split(',')
+                
+                if len(parts) >= 2: 
+                    t_number = parts[0].strip() 
+                    score = parts[1].strip()
+
+                    student_index = self.find_student(t_number)
+
+                    if student_index != -1:
+                        self.Studentlist[student_index].Grades.append(score) 
+
+#######################################
+#mAin
+STUDENTFILE = "11.Project/11.Project Students.txt"
+SCOREFILE = "11.Project/11.Project Scores.txt"
+
+def main():
+    student_list_manager = StudentList()
+    student_list_manager.add_student_from_file(STUDENTFILE)
+    student_list_manager.add_scores_from_file(SCOREFILE)
+    student_list_manager.print_student_list()
+
+if __name__ == "__main__":
+    main()
